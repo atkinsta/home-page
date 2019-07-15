8707f6f7c97aa2d6439fbc2c5b5770c8e8024677
atkinsta <atkinstaa@gmail.com>
2019-07-15 11:14:33 -0400

    Removing some other stuff

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,10 +8,10 @@ class HomePage extends Component {
     render() {
         return (
             <div>
-                <Sidebar />
                 <Box1>
-                    <p> This website is currently under construction... </p>
-                    <p>Please view my <a href="http://www.github.com/atkinsta">github profile</a> for a portfolio of my work</p>
+                    <p style={{color:"white", textAlign: "center"}}> This website is currently under construction... </p>
+                    <p style={{color:"white", textAlign: "center"}}>Please view my <a href="http://www.github.com/atkinsta">github profile</a> for a portfolio of my work</p>
+                    
                 </Box1>
             </div>
         )

b417483c9de8bdcdc9c61d0dd20cbacde2429d22
atkinsta <atkinstaa@gmail.com>
2019-07-29 22:14:42 -0400

    added resume page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -8,6 +8,7 @@ class HomePage extends Component {
                 <div className="center">
                     <p style={{color:"white", textAlign: "center"}}> This website is currently under construction... </p>
                     <p style={{color:"white", textAlign: "center"}}>Please view my <a href="http://www.github.com/atkinsta">github profile</a> for a portfolio of my work</p>
+                    <p style={{color:"white", textAlign: "center"}}>or click <a href="/resume">here</a> to view my resume as a web rendered PDF.</p>
                 </div>
             </div>
         )
